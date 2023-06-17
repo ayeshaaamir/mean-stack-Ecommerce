103ff658bd7b340a72acaadd98920bfbf11961fd
Ayesha Aamir <ayeshaaamir38@gmail.com>
2023-06-17 23:53:25 +0500

    login functionality added..

---
+++ frontend/src/app/components/pages/login-page/login-page.component.ts
@@ -0,0 +1,47 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { UserService } from '../../../services/user.service';
+import { ActivatedRoute, Router } from '@angular/router';
+
+@Component({
+  selector: 'app-login-page',
+  templateUrl: './login-page.component.html',
+  styleUrls: ['./login-page.component.css'],
+})
+export class LoginPageComponent implements OnInit {
+  loginForm!: FormGroup;
+  isSubmitted: boolean = false;
+  returnUrl: string = '';
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private userService: UserService,
+    private activatedRoute: ActivatedRoute,
+    private router: Router
+  ) {}
+
+  ngOnInit(): void {
+    this.loginForm = this.formBuilder.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required]],
+    });
+    this.returnUrl = this.activatedRoute.snapshot.queryParams.returnUrl;
+  }
+
+  get fc() {
+    return this.loginForm.controls;
+  }
+
+  submit() {
+    if (this.loginForm.invalid) return;
+    this.isSubmitted = true;
+    this.userService
+      .login({
+        email: this.fc.email.value,
+        password: this.fc.password.value,
+      })
+      .subscribe(() => {
+        this.router.navigateByUrl(this.returnUrl);
+      });
+  }
+}
